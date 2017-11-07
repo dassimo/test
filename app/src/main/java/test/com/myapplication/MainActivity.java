3ad493e04ad3d0432487e8eca8a09181d28b0fda
houssin boulla <h.boulla@gmail.com>
2017-11-07 10:32:08 +0000

    first commit depuis oldMac

--- app/src/main/java/test/com/myapplication/MainActivity.java
+++ app/src/main/java/test/com/myapplication/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //test commit git account MAC.
+        // comment depuis oldMac
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
